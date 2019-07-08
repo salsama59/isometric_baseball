c644b939fd86fb3f9a19b2c87651414e3d754567
Saül Yéponde <saul.yeponde@gmail.com>
2019-07-08 17:24:37 +0200

    Add player statistics
    Added new fields in the PlayerStatus class.

--- Assets/Scripts/DataObjects/PlayerStatus.cs
+++ Assets/Scripts/DataObjects/PlayerStatus.cs
@@ -7,8 +7,26 @@ public class PlayerStatus
     private PlayerFieldPositionEnum playerFieldPosition;
     private string playerName;
     private bool isAllowedToMove = false;
+    private float speed;
+    private float catchEfficiency;
+    private float pitchEfficiency;
+    private float passEfficiency;
+    private float battingEfficiency;
+    private int battingPower;
+    private int pitchingPower;
+    private float pitchingEffect;
+    private int stamina;
 
     public PlayerFieldPositionEnum PlayerFieldPosition { get => playerFieldPosition; set => playerFieldPosition = value; }
     public string PlayerName { get => playerName; set => playerName = value; }
     public bool IsAllowedToMove { get => isAllowedToMove; set => isAllowedToMove = value; }
+    public float Speed { get => speed; set => speed = value; }
+    public float CatchEfficiency { get => catchEfficiency; set => catchEfficiency = value; }
+    public float PitchEfficiency { get => pitchEfficiency; set => pitchEfficiency = value; }
+    public float PassEfficiency { get => passEfficiency; set => passEfficiency = value; }
+    public float BattingEfficiency { get => battingEfficiency; set => battingEfficiency = value; }
+    public int BattingPower { get => battingPower; set => battingPower = value; }
+    public int PitchingPower { get => pitchingPower; set => pitchingPower = value; }
+    public float PitchingEffect { get => pitchingEffect; set => pitchingEffect = value; }
+    public int Stamina { get => stamina; set => stamina = value; }
 }
